3e907011e009a7dc7bd5e7dd6d836f1f571f9f3d
davidkupyn <71174027+davidkupyn@users.noreply.github.com>
2023-07-12 00:56:23 +0200

    Update modal-content.svelte to fix warping to the top of the page

--- src/lib/ui/modal/modal-content.svelte
+++ src/lib/ui/modal/modal-content.svelte
@@ -11,29 +11,28 @@
 	export { className as class };
 </script>
 
-<div
-	use:portal
-	class={cn($open && 'z-50 flex justify-center sm:items-center items-end fixed inset-0')}
->
+<div use:portal>
 	{#if $open}
-		<div
-			{...$overlay}
-			transition:fade={{ duration: 150 }}
-			class="fixed z-50 inset-0 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] sm:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-base-600/50 to-base-600/95 dark:from-base-950/70 dark:to-base-950/95"
-		/>
-		<div
-			transition:scale={{ duration: 200, start: 0.95 }}
-			class={cn(modal({ alert }).base(), className)}
-			{...$content}
-			use:content
-		>
-			<slot {Title} {Description} close={$close} />
-
-			{#if crossButton && !alert}
-				<button {...$close} use:close class="absolute right-4 top-4 btn btn-sm btn-ghost btn-icon">
-					<X size=16 />
-				</button>
-			{/if}
+		<div class={cn($open && 'z-50 flex justify-center sm:items-center items-end fixed inset-0')}>
+			<div
+				{...$overlay}
+				transition:fade={{ duration: 150 }}
+				class="fixed z-50 inset-0 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] sm:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-base-600/50 to-base-600/95 dark:from-base-950/70 dark:to-base-950/95"
+			/>
+			<div
+				transition:scale={{ duration: 200, start: 0.95 }}
+				class={cn(modal({ alert }).base(), className)}
+				{...$content}
+				use:content
+			>
+				<slot {Title} {Description} close={$close} />
+	
+				{#if crossButton && !alert}
+					<button {...$close} use:close class="absolute right-4 top-4 btn btn-sm btn-ghost btn-icon">
+						<X size=16 />
+					</button>
+				{/if}
+			</div>
 		</div>
 	{/if}
 </div>
