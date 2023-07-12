73dee51038b819553dea988331bc8025bedce0e7
davidkupyn <davidkupyn@gmail.com>
2023-07-12 19:17:19 +0200

    Change slider styles to accent color

--- src/lib/ui/slider.svelte
+++ src/lib/ui/slider.svelte
@@ -21,15 +21,15 @@
 </script>
  
 <span {...$slider} class={cn("group relative flex w-full touch-none select-none items-center", className)}>
-  <span class="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-    <span {...$range} class="absolute h-full bg-primary" />
+  <span class="relative h-1.5 w-full grow overflow-hidden rounded-full bg-accent-500/20">
+    <span {...$range} class="absolute h-full bg-accent" />
   </span>
  
   {#each { length: $valueStore.length } as _}
     <span
       {...$thumb()}
       use:thumb
-      class="block h-4 w-4 rounded-full focus:outline-0 border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring group-disabled:pointer-events-none group-disabled:opacity-50"
+      class="block h-4 w-4 rounded-full focus-visible:outline-none border border-accent-500/50 bg-background shadow transition-colors focus-visible:ring-1 focus-visible:ring-accent group-disabled:pointer-events-none group-disabled:opacity-50"
     />
   {/each}
 </span>
