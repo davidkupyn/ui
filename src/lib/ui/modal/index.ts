fc29650e683ec16d25ceb5ca0dd4a3e2b7d7fe73
davidkupyn <71174027+davidkupyn@users.noreply.github.com>
2023-07-15 17:43:27 +0200

    Update index.ts

--- src/lib/ui/modal/index.ts
+++ src/lib/ui/modal/index.ts
@@ -6,7 +6,7 @@ export { default as Modal } from './modal-root.svelte';
 
 export const modal = tv({
 	slots: {
-		base: 'relative z-50 grid w-full sm:max-w-lg shadow-lg focus:outline-none rounded-3xl border border-popover-border bg-popover p-6 gap-6 rounded-t-3xl [--modal-scale:1] sm:[--modal-scale:0.95] [--modal-y:10rem] sm:[--modal-y:0px] [--modal-duration:200] dark:[box-shadow:_inset_0_1px_0.5px_0px_hsl(0_0%_100%/0.06)]',
+		base: 'relative z-50 grid w-full sm:max-w-lg shadow-lg focus:outline-none rounded-3xl border border-popover-border bg-popover p-6 gap-6 rounded-t-3xl [--modal-scale:1] sm:[--modal-scale:0.95] [--modal-y:10rem] sm:[--modal-y:0px] [--modal-duration:300] sm:[--modal-duration:200] dark:[box-shadow:_inset_0_1px_0.5px_0px_hsl(0_0%_100%/0.06)]',
 		title: 'font-semibold text-md sm:text-lg leading-none tracking-tight flex items-center',
 		description: 'text-sm text-muted-foreground'
 	},
