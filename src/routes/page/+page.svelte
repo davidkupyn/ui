514d1c050909d80acd7d9644f342d5fca6ac4f42
davidkupyn <davidkupyn@gmail.com>
2023-07-21 15:43:04 +0200

    Remove the default theme color in layout

--- src/routes/page/+page.svelte
+++ src/routes/page/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Button from '$lib/ui/button.svelte';
 	import {tweened} from 'svelte/motion';
 	
 	let colorIndex = 0;
@@ -10,7 +11,7 @@
 	const decimalToHex = decimal => Math.round(decimal).toString(16).padStart(2, '0');
 	
 	// This cycles through the indexes of the colors array.
-	const goToNextColor = () => colorIndex = (colorIndex + 1) % colors.length;
+	const goToNextColor = () => colorIndex = (colorIndex + 1) % colorsDark.length;
 	
 	// This extracts two hex characters from an "rrggbb" color string
 	// and returns the value as a number between 0 and 255.
@@ -41,8 +42,8 @@
 	}
 	
 	// Create a tweened store that holds an "rrggbb" hex color.
-	const colorLight = tweened(colorsLight[colorIndex], {duration: 300, interpolate: rgbInterpolate});
-	const colorDark = tweened(colorsDark[colorIndex], {duration: 300, interpolate: rgbInterpolate});
+	const colorLight = tweened(colorsLight[colorIndex], {duration: 150, interpolate: rgbInterpolate});
+	const colorDark = tweened(colorsDark[colorIndex], {duration: 150, interpolate: rgbInterpolate});
 	// Trigger tweening if colorIndex changes.
 	$: colorLight.set(colorsLight[colorIndex]);
 	$: colorDark.set(colorsDark[colorIndex]);
@@ -53,6 +54,9 @@
   <meta name="theme-color" content="#{$colorDark}" media="(prefers-color-scheme: dark)"/>
 </svelte:head>
 
-<button class="mt-24" on:click={goToNextColor}>Next</button>
-<span>color = {$color}</span>
-<h1 style="color: #{$color}">Tweened Color</h1>
+<Button class="mt-24" on:click={goToNextColor}>Next</Button>
+<!-- <span>color = {$colorDark}</span>
+<span>color = {$colorLight}</span> -->
+
+<h1 style="color: #{$colorDark}">Tweened Color</h1>
+<h1 style="color: #{$colorLight}">Tweened Color</h1>
