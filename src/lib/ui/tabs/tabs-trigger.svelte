534037dfa167f2376d64845c8ee96469d68538fb
davidkupyn <71174027+davidkupyn@users.noreply.github.com>
2023-07-26 22:31:02 +0200

    Update tabs-trigger.svelte

--- src/lib/ui/tabs/tabs-trigger.svelte
+++ src/lib/ui/tabs/tabs-trigger.svelte
@@ -30,7 +30,7 @@
 			in:receive={{ key: 'tab', duration: 200 }}
 			out:send={{ key: 'tab', duration: 200 }}
 			class={cn(
-				'w-full h-full group-data-[orientation=vertical]:rounded-xl rounded-lg overflow-hidden bg-background shadow dark:shadow-[inset_0_-1px_0_#ffffff0f]'
+				'w-full h-full group-data-[orientation=vertical]:rounded-xl rounded-lg overflow-hidden bg-background shadow dark:shadow-[inset_0_-1px_0_#ffffff5f]'
 				// transparent && 'bg-background shadow'
 			)}
 		/>
